cab805f223cfa47ea1e08dc6e20d0c4b1b7df372
TrasherTravis <trashertravis@gmail.com>
2021-06-20 23:04:29 +0530

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,98 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://drivenx-main.netlify.app/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://drivenx-main.netlify.app/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    // href: '/nests',
+    href:'https://drivenx-main.netlify.app/pools'
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'DrivenX',
+        href: 'http://www.drivenx.finance',
+      },
+      {
+        label: 'Discord',
+        href: 'https://discord.com/invite/drivenprotocol',
+      },
+      {
+        label: 'Instagram',
+        href: 'https://www.instagram.com/drivenprotocol',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/drivenx/',
+      },
+      {
+        label: 'BSCSCAN',
+        href: 'https://bscscan.com/token/0x0a950ac7f14a4982cee3bf24adb1dad17e0a9026',
+      },
+    ],
+  },
+  // {
+  //   label: 'Partnerships/IFO',
+  //   icon: 'GooseIcon',
+  //   href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+  // },
+  // {
+  //   label: 'Audit by Hacken',
+  //   icon: 'AuditIcon',
+  //   href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+  // },
+  // {
+  //   label: 'Audit by CertiK',
+  //   icon: 'AuditIcon',
+  //   href: 'https://certik.org/projects/goose-finance',
+  // },
+]
+
+export default config
---
+++ src/constants/DVXPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, wbnbAddress: string, lpAddress:string} = {
+  cakeAddress: '0x0a950ac7f14a4982cee3bf24adb1dad17e0a9026',
+  wbnbAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+  lpAddress: '0x43a599b5572327a316B3c71DFE62C53d1C05E108'
+}
+
+export default priceContracts
